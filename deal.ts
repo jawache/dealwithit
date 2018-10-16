77fa167bbfae37c46cc40e08241fea1318182a91
jawache <jawache@gmail..com>
2018-10-16 14:53:03 +0100

    Completed ❤️

--- deal.ts
+++ deal.ts
@@ -1,8 +1,9 @@
 import * as Jimp from "jimp";
 import fetch from "node-fetch";
+import * as path from "path";
 
-const API_URL = "...";
-const API_KEY = "...";
+const API_URL = "<URL>/detect?returnFaceLandmarks=true";
+const API_KEY = "<KEY>";
 
 async function detectFaces(imageUrl) {
   let response = await fetch(API_URL, {
@@ -18,7 +19,23 @@ async function detectFaces(imageUrl) {
   return resp;
 }
 
-async function processImage(imageUrl, faces) {}
+async function processImage(imageUrl, faces) {
+  let image = await Jimp.read(imageUrl);
+  let origShades = await Jimp.read("./assets/shades.png");
+
+  for (let face of faces) {
+    let shades = origShades.clone();
+    let {
+      faceRectangle: { left, width },
+      faceLandmarks: { pupilLeft, pupilRight }
+    } = face;
+    const eyeHeight = (pupilLeft.y + pupilRight.y) / 2.0;
+    shades.resize(width, Jimp.AUTO);
+    const shadesHeight = eyeHeight - shades.bitmap.height / 2.0;
+    image = image.composite(shades, left, shadesHeight);
+  }
+  image.write(path.join(__dirname, "./out/", `thuged.jpg`));
+}
 
 async function main() {
   const [imageUrl] = process.argv.slice(2);
